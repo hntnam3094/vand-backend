599110ac5c711f0646941c31dc4274f4696bb8b4
Thanh Nam <hntnam98@gmail.com>
2023-05-07 13:33:02 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/storage_link', function () {
+    Artisan::call('storage:link');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
